f7928097c79a4513d519d9fdd3dd7283346bfcae
Simon Ho <simonho@ca.ibm.com>
2016-01-26 23:28:46 -0800

    Move validations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.ModelBuilder = exports.LDL = require('loopback-model/lib/model-builder')
 exports.DataSource = exports.Schema = require('./lib/datasource.js').DataSource;
 exports.ModelBaseClass = require('./lib/model.js');
 exports.GeoPoint = require('loopback-model').Geo.GeoPoint;
-exports.ValidationError = require('./lib/validations.js').ValidationError;
+exports.ValidationError = require('loopback-model/lib/validations').ValidationError;
 
 Object.defineProperty(exports, 'version', {
   get: function() {return require('./package.json').version;}
